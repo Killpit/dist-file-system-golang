4aa15442bf06afd6e81876ffc6eabfc8b1b34294
Ata Tekeli <tekeliata2000@gmail.com>
2024-08-20 16:37:05 +0300

    at code cleaning

--- store_test.go
+++ store_test.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"bytes"
-	"fmt"
 	"io"
 	"testing"
 )
@@ -18,6 +17,23 @@ func TestPathTransformFunc(t *testing.T) {
 	}
 	if pathKey.Filename != expectedPathName {
 		t.Error(t, "have %s want %", pathKey.Filename, expectedOriginalKey)
+	}
+}
+
+func TestStoreDeleteKey(t *testing.T) {
+	opts := StoreOpts {
+		PathTransformFunc: CASPathTransformFunc,
+	}
+	s := NewStore(opts)
+	key := "momsspecials"
+	data := []byte("some jpg bytes")
+
+	if err := s.writeStream(key, bytes.NewReader(data)); err != nil {
+		t.Error(err)
+	}
+
+	if err := s.Delete(key); err != nil {
+		t.Error(err)
 	}
 }
 
@@ -39,11 +55,9 @@ func TestStore(t *testing.T) {
 	}
 
 	b, _ := io.ReadAll(r)
-
-	fmt.Println(string(b))
-
 	if string(b) != string(data) {
 		t.Errorf("want %s have %s", data, b)
 	}
 
+	//s.Delete(key)
 }
